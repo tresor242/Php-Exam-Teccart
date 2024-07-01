77fba5a2472c315072f006adebe4d477f188e187
Tresor Elila <elilatresor25@gmail.com>
2024-07-01 11:16:06 -0400

    PHP ECOM TECCART

---
+++ includes/secondfooter.php
@@ -0,0 +1,37 @@
+<footer class="page-footer">
+    <div class="container footer-info">
+        <div class="row">
+            <div class="col s12 l4 ">
+                <h5 class="grey-text text-lighten-3 animated slideInUp wow">Developers</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="https://github.com/jacobjohn2016"><i class="tiny material-icons" style="position:relative;top:3px">developer_mode</i> Jacob John</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="https://smakosh.com/"><i class="tiny material-icons" style="position:relative;top:3px">developer_mode</i>Ismail Ghallou</a></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="col s12 l4 ">
+                <h5 class="grey-text text-lighten-3 animated slideInUp wow">Categories</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartPhone</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartCam</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartLaptop</a></li>
+                </ul>
+            </div>
+
+            <div class="col s12 l4 ">
+                <h5 class="grey-text text-lighten-3 animated slideInUp wow"></h5></br>
+                <ul>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartWatch</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartSpeaker</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartVR</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © <?= Date('Y'); ?> All rights reserved
+            <a class="grey-text text-lighten-4 right animated slideInUp wow" href="https://smakosh.com/"><i class="fa fa-heartbeat animated pulse infinite red-text"></i>Ismail Ghallou and Jacob John</a>
+        </div>
+    </div>
+</footer>
